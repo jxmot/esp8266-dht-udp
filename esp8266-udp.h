fae88c26c33ba970af2f03b17a7ee31e1f47aea2
jxmot <webdev@stupiddog.net>
2017-12-22 17:28:12 -0600

    sketch udp support functions

---
+++ esp8266-udp.h
@@ -0,0 +1,30 @@
+/* ************************************************************************ */
+/*
+    esp8266-udp.h - UDP support for the associated ino file.
+
+*/
+#pragma once
+
+#include "udp-defs.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int initUDP();
+
+
+
+extern int sendUDP(char *payload, int len);
+extern int replyUDP(char *payload, int len);
+extern int recvUDP();
+
+extern int multiUDP(char *payload, int len);
+
+extern unsigned char readBuffer[];
+extern unsigned char writeBuffer[];
+
+#ifdef __cplusplus
+}
+#endif
+
